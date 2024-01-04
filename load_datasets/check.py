939bc6e1631f04da62c417be9aa87d62859099db
Yuchen Zeng <yzeng58@wisc.edu>
2024-01-03 19:40:24 -0600

    clean unused functions

--- load_datasets/check.py
+++ load_datasets/check.py
@@ -20,48 +20,6 @@ llava_tokenizer, llava_model, llava_image_processor, llava_context_len, llava_ar
 qwen_tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen-VL-Chat", trust_remote_code=True)
 qwen_model = AutoModelForCausalLM.from_pretrained("Qwen/Qwen-VL-Chat", device_map='cuda:0', trust_remote_code=True).eval()
 qwen_model.generation_config = GenerationConfig.from_pretrained("Qwen/Qwen-VL-Chat", trust_remote_code=True)
-
-def generate_single_caption(image_path):
-    prompt = f"Generate a clear description of the image <image-placeholder>"
-    caption = eval_model(
-        prompt,
-        [image_path],
-        llava_tokenizer,
-        llava_model,
-        llava_image_processor,
-        llava_context_len,
-        llava_args,
-        device='cuda',
-    )
-    
-    return caption
-
-def generate_captions(
-    dataset_folder_path,
-    overwrite = False,
-    test = False,
-):
-    # recursively find all images
-    for root, dirs, files in os.walk(dataset_folder_path):
-        print(f"Processing {os.path.split(root)[0]}...")
-        for file in files:
-            if file.endswith(".jpg"):
-                
-                image_path = os.path.join(root, file)
-                caption_path = image_path.replace('datasets', 'results/captions').replace('.jpg', '.txt')
-                folder = os.path.dirname(caption_path)
-                if not os.path.exists(folder):
-                    os.makedirs(folder)
-                    
-                if os.path.exists(caption_path) and not overwrite:
-                    continue
-            
-                caption = generate_single_caption(image_path)
-                print(f"Caption for {file}: {caption}")
-                with open(caption_path, 'w') as f:
-                    f.write(caption)
-                    
-                if test: break
     
 def check_caption(task_type):
     category_space = {}
@@ -257,7 +215,6 @@ def check_image(task_type):
 if '__main__' == __name__:
     parser = argparse.ArgumentParser(description='Generate image descriptions for the dataset')
     parser.add_argument('--mode', type=str, default = 'image', help='what check to do', choices = ['image', 'caption'])
-    parser.add_argument('--overwrite', type=int, default = 0, help='overwrite existing captions or not', choices = [0,1])
     parser.add_argument('--task_type', type=str, nargs='+', default = task_types, help='what task to check', choices = task_types)
     
     args = parser.parse_args()
@@ -265,11 +222,6 @@ if '__main__' == __name__:
     set_seed(123)
     
     if args.mode == 'caption':
-        # generate_captions(
-        #     f"{root_dir}/datasets",
-        #     overwrite = args.overwrite,
-        #     test = args.test,
-        # )
         for task_type in args.task_type:
             check_caption(task_type)
     elif args.mode == 'image':
